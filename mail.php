8c086b108e856bce99edc9dcca6c40ab8726f16b
Seun Oyewole <oyewoleo@uni.coventry.ac.uk>
2017-02-26 23:37:20 +0000

    contact form testing

---
+++ mail.php
@@ -0,0 +1,41 @@
+<?php
+if(isset($_POST['submit'])) 
+{
+
+$message=
+'Full Name:	'.$_POST['fullname'].'<br />
+Subject:	'.$_POST['subject'].'<br />
+Email:	'.$_POST['emailid'].'<br />
+Comments:	'.$_POST['comments'].'
+';
+    require "phpmailer/class.phpmailer.php"; //include phpmailer class
+      
+    // Instantiate Class  
+    $mail = new PHPMailer();  
+      
+    // Set up SMTP  
+    $mail->IsSMTP();                // Sets up a SMTP connection  
+    $mail->SMTPAuth = true;         // Connection with the SMTP does require authorization    
+    $mail->SMTPSecure = "ssl";      // Connect using a TLS connection  
+    $mail->Host = "n3plcpnl0054.prod.ams3.secureserver.net";  //Gmail SMTP server address
+    $mail->Port = 465;  //Gmail SMTP port
+    $mail->Encoding = '7bit';
+    
+    // Authentication  
+    $mail->Username   = "noreply@noxielimited.com"; // Your full Gmail address
+    $mail->Password   = "NoxieLimited@2017"; // Your Gmail password
+      
+    // Compose
+    $mail->SetFrom($_POST['emailid'], $_POST['fullname']);
+    $mail->AddReplyTo($_POST['emailid'], $_POST['fullname']);
+    $mail->Subject = "New Contact Form Enquiry";      // Subject (which isn't required)  
+    $mail->MsgHTML($message);
+ 
+    // Send To  
+    $mail->AddAddress("noreply@noxielimited.com", "Noxie Limited"); // Where to send it - Recipient
+    $result = $mail->Send();		// Send!  
+	$message = $result ? 'Successfully Sent!' : 'Sending Failed!';      
+	unset($mail);
+
+}
+?>
