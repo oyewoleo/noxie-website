152ef40043171e6b3eca4f61ca024b0fb186fd01
Seun Oyewole <oyewoleo@uni.coventry.ac.uk>
2017-02-27 00:13:33 +0000

    ..

--- mail.php
+++ mail.php
@@ -19,7 +19,6 @@ Comments:	'.$_POST['comments'].'
     $mail->SMTPSecure = "ssl";      // Connect using a TLS connection  
     $mail->Host = "n3plcpnl0054.prod.ams3.secureserver.net";  //Gmail SMTP server address
     $mail->Port = 465;  //Gmail SMTP port
-    $mail->Encoding = '7bit';
     
     // Authentication  
     $mail->Username   = "noreply@noxielimited.com"; // Your full Gmail address
